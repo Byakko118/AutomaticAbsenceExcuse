3eaf98b398cf4f201fc2e31047b229c89f54f3f5
Alan Goźliński (Uczeń) <agozlinski352@technikumkreatywne.pl>
2025-01-30 13:14:41 +0100

    a

--- main.py
+++ main.py
@@ -8,43 +8,50 @@ import time
 USERNAME = "Alan Goźliński"
 EMAIL = "agozlinski352@technikumkreatywne.pl"
 PASSWORD = "(GoldenExpRequiem12#118)"
-TEACHER = "Jacek Konczalski"  # This can be changed later
+TEACHER = "Jacek Konczalski"
+MESSAGE = "Niech pan obleje jarosława gałeckiego proszę bo on nic nie robi na lekcjach męczy mnie i takie tam gadanie XDDDDDDDDDDDDDDDDDD"
 
 def main():
     driver = webdriver.Chrome()
     driver.implicitly_wait(25)
     driver.get("https://www.microsoft.com/pl-pl/microsoft-teams/log-in")
 
-    # Log in
     login = driver.find_element(By.CLASS_NAME, "btn ")
     login.click()
 
     windows = driver.window_handles
     driver.switch_to.window(windows[1])
 
-    email_input = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.NAME, "loginfmt")))
+    email_input = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.NAME, "loginfmt")))
     email_input.send_keys(EMAIL)
 
-    next_btn = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
+    next_btn = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
     next_btn.click()
 
-    pswd_input = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.NAME, "passwd")))
+    pswd_input = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.NAME, "passwd")))
     pswd_input.send_keys(PASSWORD)
 
-    finish_btn = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
+    finish_btn = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
     finish_btn.click()
 
-    yes_btn = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
+    yes_btn = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
     yes_btn.click()
 
-    search = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "ms-searchux-input")))
+    search = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.ID, "ms-searchux-input")))
     search.send_keys(TEACHER)
     search.send_keys(Keys.ENTER)
 
-    people = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[value='people']")))
+    people = WebDriverWait(driver, 50).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button[value='people']")))
     people.click()
 
-    time.sleep(10)  
+    li_element = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.XPATH, "//li[@aria-posinset='0']")))
+    li_element.click()
+
+    chat = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.XPATH, "//div[@aria-label='Wpisz wiadomość']")))
+    chat.send_keys(f"{MESSAGE}")
+    chat.send_keys(Keys.RETURN)
+
+    time.sleep(2)  
 
     driver.quit()
 
